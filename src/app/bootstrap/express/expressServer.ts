26e3539d0190aa5905f3ff0937a57ef0d73b03ad
Kanish Chhabra <kchhabra416@gmail.com>
2026-07-28 23:12:57 +0530

    oauth added

--- src/app/bootstrap/express/expressServer.ts
+++ src/app/bootstrap/express/expressServer.ts
@@ -28,4 +28,79 @@ export function expressServer(app:Express,PORT:number){
         console.log(`Express server is running at http://localhost:${PORT}`)
     })
 
+
+    const sess = {
+        store:MongoStore.create({
+            mongoUrl:process.env.DB_URL,
+            collectionName:"sessions"
+        }),
+        secret:process.env.COOKIE_KEY as string,
+        resave:false,
+        saveUninitialized: true,
+        cookie:{secure:false}
+    }
+
+
+    if(process.env.NODE_ENV==='production'){
+        app.set('trust proxy',1);
+        sess.cookie.secure = true;
+    }
+
+    app.use(session(sess));
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+    passport.use(
+        new GoogleStrategy(
+            {
+                clientID:process.env.GOOGLE_CLIENT_ID as string,
+                clientSecret:process.env.GOOGLE_CLIENT_SECRET as string,
+                callbackURL:process.env.CALL_BACK_URL
+            },
+            async (accessToken:string,refreshToken:string,profile:any,done:any)=>{
+                console.log("Create user...")
+                return done(null,{})
+            }
+        )
+    )
+
+
+    passport.serializeUser((user:any,done)=>{
+        done(null,user);
+    });
+
+    passport.deserializeUser(async (obj:any,done)=>{
+        try{
+            done(null,obj);
+        }catch(err){
+            done(err);
+        }
+    })
+
+    app.get(
+        "/auth/google",
+        passport.authenticate("google",{
+            scope:[
+                "profile",
+                "email"
+            ],
+            accessType:"offline",
+            prompt:"consent"
+        })
+    )
+
+    app.get(
+        "/auth/google/callback",
+        passport.authenticate("google",{
+            failureRedirect:"/auth/login",
+            successRedirect:process.env.FRONT_APP_URL
+        })
+    )
+
+    app.get("/auth/me",(req:any,res:any)=>{
+        if(!req.user) return res.status(401).json({error:"Not Logged in"});
+        res.json(req.user);
+        
+    })
+
 }
